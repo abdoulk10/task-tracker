0537dfb1ae229ed834251f12bb4db043823978ce
Abdoul <babdoul778@gmail.com>
2023-07-25 11:10:43 -0400

    Feature 16 complete

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
 from tasks.forms import TaskForm
+from tasks.models import Task
 
 # Create your views here.
 
@@ -20,7 +21,7 @@ def create_task(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
-            task = form.save()
+            form.save()
             return redirect("list_projects")
 
     else:
@@ -30,3 +31,12 @@ def create_task(request):
         "form": form,
     }
     return render(request, "tasks/create.html", context)
+
+
+@login_required
+def show_my_tasks(request):
+    tasks = Task.objects.filter(assignee=request.user)
+    context = {
+        "tasks": tasks,
+    }
+    return render(request, "tasks/list.html", context)
